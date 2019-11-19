182bafccce564b3b0635cbb5568e51bbfb4a3e0c
momo <callmedevmomo@gmail.com>
2019-11-19 13:47:39 +0900

    typing in typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// typing in typescript :  "noImplicitAny": false (in json)
+
+// const plus = (a:string,b:string) => a+b;
+
 ReactDOM.render(<App />, document.getElementById("root"));
