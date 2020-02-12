1e57a261b557a3fd3d9fc65c1acda85547c9b0f5
momo <callmedevmomo@gmail.com>
2020-02-12 18:09:40 +0900

    declare styled-components

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.span``;
+// functional Component ==> set Interface
+// styled Component ==> inline Props
+
+const Container = styled.span<{ isBlue: boolean }>`
+  color: ${props => (props.isBlue ? props.theme.blueColor : "black")};
+`;
 interface IProps {
   count: number;
 }
 const Number: React.FunctionComponent<IProps> = ({ count }) => (
-  <Container>{count}</Container>
+  <Container isBlue={count > 10}>{count}</Container>
 );
 
 export default Number;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 // typing in typescript :  "noImplicitAny": false (tsconfig.json)
 
 // const plus = (a:string,b:string) => a+b;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
